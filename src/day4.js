be6da1701a918a9397323f7546d9132912365d54
Kevin Parrott <kevin.parrott@detroitlabs.com>
2017-12-04 12:43:02 -0500

    set's have size

--- src/day4.js
+++ src/day4.js
@@ -1,14 +1,14 @@
 const part1 = (input) => {
   const result = input.reduce((sum, str) => {
-    return str.split(' ').length === [...new Set(str.split(' '))].length ? sum + 1 : sum
+    return str.split(' ').length === new Set(str.split(' ')).size ? sum + 1 : sum
   }, 0)
   return result
 }
 
 const part2 = (input) => {
   const result = input.reduce((sum, str) => {
-    const deduped = [...new Set(str.split(' ').map(s => s.split('').sort().join('')))]
-    return str.split(' ').length === deduped.length ? sum + 1 : sum
+    const deduped = new Set(str.split(' ').map(s => s.split('').sort().join('')))
+    return str.split(' ').length === deduped.size ? sum + 1 : sum
   }, 0)
   return result
 }
