82e605db17b7052edaf4872b396f130a4e5a23c0
Kevin Parrott <parrott.kevin@gmail.com>
2017-12-05 00:43:14 -0500

    day 5 complete

--- src/day-05/index.js
+++ src/day-05/index.js
@@ -1,9 +1,27 @@
-const part1 = () => {
-
+const traverse = (instructions, increment) => {
+  let done = false
+  let i = 0
+  let steps = 0
+  let jump = instructions[i]
+  while (!done) {
+    if (i >= instructions.length) {
+      done = true
+    } else {
+      steps += 1
+      jump = instructions[i]
+      instructions[i] += increment(jump)
+      i += jump
+    }
+  }
+  return steps
 }
 
-const part2 = () => {
+const part1 = (input) => {
+  return traverse(input, () => 1)
+}
 
+const part2 = (input) => {
+  return traverse(input, jump => jump >= 3 ? -1 : 1)
 }
 
 module.exports = { part1, part2 }
--- src/day-05/index.test.js
+++ src/day-05/index.test.js
@@ -5,37 +5,39 @@ const path = require('path')
 const { part1, part2 } = require('./index')
 
 const readFile = util.promisify(fs.readFile)
-const puzzleInput = async () => (await readFile(path.join(__dirname, './puzzleInput.txt'), 'utf8')).split(/\n/)
+const puzzleInput = async () => {
+  return (await readFile(path.join(__dirname, './puzzleInput.txt'), 'utf8')).split(/\n/).map(i => parseInt(i, 10))
+}
 
-describe('day 4', () => {
+describe('day 5', () => {
   describe('part 1', () => {
     test('example', () => {
-      const input = ''
+      const input = [0, 3, 0, 1, -3]
       const result = part1(input)
-      const answer = ''
+      const answer = 5
       expect(result).toEqual(answer)
     })
 
-    test('puzzle', () => {
-      const input = ''
+    test('puzzle', async () => {
+      const input = await puzzleInput()
       const result = part1(input)
-      const answer = ''
+      const answer = 342669
       expect(result).toEqual(answer)
     })
   })
 
   describe('part 2', () => {
     test('example', () => {
-      const input = ''
-      const result = part1(input)
-      const answer = ''
+      const input = [0, 3, 0, 1, -3]
+      const result = part2(input)
+      const answer = 10
       expect(result).toEqual(answer)
     })
 
-    test('puzzle', () => {
-      const input = ''
-      const result = part1(input)
-      const answer = ''
+    test('puzzle', async () => {
+      const input = await puzzleInput()
+      const result = part2(input)
+      const answer = 25136209
       expect(result).toEqual(answer)
     })
   })
