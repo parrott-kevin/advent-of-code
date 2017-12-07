8a24566fc2b8e9e5809ac12328eec0063f63a48c
Kevin Parrott <parrott.kevin@gmail.com>
2017-12-07 08:16:49 -0500

    day 7 part 1 complete

--- src/day-07/index.js
+++ src/day-07/index.js
@@ -1,8 +1,79 @@
-const part1 = () => {
-  return true
+const findParent = (obj, node) => {
+  let parent
+  const keys = Object.keys(obj)
+  for (let i = 0; i < keys.length; i++) {
+    let visitedNode = obj[keys[i]]
+    if (visitedNode.children.includes(node.program)) {
+      parent = visitedNode
+      break
+    }
+  }
+
+  if (parent) {
+    return findParent(obj, parent)
+  } else {
+    return node
+  }
 }
 
-const part2 = () => {
+class Queue {
+  constructor () {
+    this.storage = {}
+    this.oldIndex = 0
+    this.newIndex = 0
+  }
+
+  size () {
+    return this.newIndex - this.oldIndex
+  }
+
+  enqueue (node) {
+    this.storage[this.newIndex] = node
+    this.newIndex++
+  }
+
+  dequeue () {
+    const result = this.storage[this.oldIndex]
+
+    delete this.storage[this.oldIndex]
+    this.oldIndex++
+
+    return result
+  }
+}
+
+// need to determine level of tree you are currently on then store that info.
+// when finished determining levels start from deepest level and sum all nodes
+// with the same parent.
+const breadthFirst = (graph, root) => {
+  root.marked = true
+
+  let q = new Queue()
+  q.enqueue(root)
+  while (q.size() > 0) {
+    const node = q.dequeue()
+    console.log(node)
+
+    node.children.forEach(n => {
+      const adjNode = graph[n]
+      if (!adjNode.marked) {
+        adjNode.marked = true
+        q.enqueue(adjNode)
+      }
+    })
+  }
+}
+
+const part1 = (input) => {
+  let node = input[Object.keys(input)[0]]
+  const results = findParent(input, node)
+  return results
+}
+
+const part2 = (input) => {
+  let node = input[Object.keys(input)[0]]
+  let root = findParent(input, node)
+  breadthFirst(input, root)
   return true
 }
 
--- src/day-07/index.test.js
+++ src/day-07/index.test.js
@@ -6,39 +6,82 @@ const { part1, part2 } = require('./index')
 
 const readFile = util.promisify(fs.readFile)
 const puzzleInput = async () => {
-  return (await readFile(path.join(__dirname, './puzzleInput.txt'), 'utf8')).split(/\n/).map(i => parseInt(i, 10))
+  return (await readFile(path.join(__dirname, './puzzleInput.txt'), 'utf8')).split(/\n/)
+}
+
+const decompose = (ary) => {
+  return ary.reduce((result, i) => {
+    const parentChild = i.split('->')
+    const children = parentChild.length > 1 ? parentChild[1].split(',').map(c => c.trim()) : []
+    const programWeight = parentChild[0].split('(')
+    const program = programWeight[0].trim()
+    const weight = parseInt(programWeight[1].slice(0, -1), 10)
+    result[program] = Object.assign({}, {
+      program,
+      weight,
+      children
+    })
+    return result
+  }, {})
 }
 
 describe('day XXX', () => {
   describe('part 1', () => {
     test('example', () => {
-      const input = []
-      const result = part1(input)
-      const answer = true
-      expect(result).toEqual(answer)
+      const input = `pbga (66)
+      xhth (57)
+      ebii (61)
+      havc (66)
+      ktlj (57)
+      fwft (72) -> ktlj, cntj, xhth
+      qoyq (66)
+      padx (45) -> pbga, havc, qoyq
+      tknk (41) -> ugml, padx, fwft
+      jptl (61)
+      ugml (68) -> gyxo, ebii, jptl
+      gyxo (61)
+      cntj (57)`
+      const decomposedInput = decompose(input.split(/\n/))
+      const result = part1(decomposedInput)
+      const answer = 'tknk'
+      expect(result.program).toEqual(answer)
     })
 
     test('puzzle', async () => {
       const input = await puzzleInput()
-      const result = part1(input)
-      const answer = true
-      expect(result).toEqual(answer)
+      const decomposedInput = decompose(input)
+      const result = part1(decomposedInput)
+      const answer = 'hmvwl'
+      expect(result.program).toEqual(answer)
     })
   })
 
   describe('part 2', () => {
     test('example', () => {
-      const input = []
-      const result = part2(input)
-      const answer = true
-      expect(result).toEqual(answer)
+      const input = `pbga (66)
+      xhth (57)
+      ebii (61)
+      havc (66)
+      ktlj (57)
+      fwft (72) -> ktlj, cntj, xhth
+      qoyq (66)
+      padx (45) -> pbga, havc, qoyq
+      tknk (41) -> ugml, padx, fwft
+      jptl (61)
+      ugml (68) -> gyxo, ebii, jptl
+      gyxo (61)
+      cntj (57)`
+      const decomposedInput = decompose(input.split(/\n/))
+      const result = part2(decomposedInput)
+      const answer = 'tknk'
+      expect(result.program).toEqual(answer)
     })
 
     test('puzzle', async () => {
-      const input = await puzzleInput()
-      const result = part2(input)
-      const answer = true
-      expect(result).toEqual(answer)
+      // const input = await puzzleInput()
+      // const result = part2(input)
+      // const answer = true
+      // expect(result).toEqual(answer)
     })
   })
 })
