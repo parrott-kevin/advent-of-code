5482c99ff23b8f81c92ae7d3974c596147b0895d
Kevin Parrott <parrott.kevin@gmail.com>
2017-12-10 02:00:39 -0500

    day 9 solved

--- src/day-09/index.js
+++ src/day-09/index.js
@@ -1,9 +1,40 @@
-const part1 = () => {
-  return true
+const takeOutGarbage = (str, count = 0) => {
+  const end = str.indexOf('>')
+  if (end >= 1) {
+    const start = (str.slice(0, end)).indexOf('<')
+    const sliced = str.slice(0, start) + str.slice(end + 1)
+    count += (end - start - 1)
+    return takeOutGarbage(sliced, count)
+  } else {
+    return { str, count }
+  }
 }
 
-const part2 = () => {
-  return true
+const hike = (str) => {
+  const storage = {}
+  let level = 1
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '{') {
+      !(level in storage) && (storage[level] = 0)
+      storage[level] = storage[level] + 1
+      level += 1
+    }
+    if (str[i] === '}') {
+      level -= 1
+    }
+  }
+  const sum = Object.keys(storage).reduce((r, key) => {
+    r += parseInt(key, 10) * storage[key]
+    return r
+  }, 0)
+  return sum
 }
 
-module.exports = { part1, part2 }
+const main = (input) => {
+  const removedIgnored = input.replace(/!./g, '')
+  const removedGarbage = takeOutGarbage(removedIgnored)
+  const cleaned = removedGarbage.str.replace(/{,}/g, '{}').replace(/{,{/g, '{{').replace(/},}/g, '}}')
+  return { score: hike(cleaned), garbageCount: removedGarbage.count }
+}
+
+module.exports = { main }
--- src/day-09/index.test.js
+++ src/day-09/index.test.js
@@ -2,43 +2,31 @@ const fs = require('fs')
 const util = require('util')
 const path = require('path')
 
-const { part1, part2 } = require('./index')
+const { main } = require('./index')
 
 const readFile = util.promisify(fs.readFile)
 const puzzleInput = async () => {
-  return (await readFile(path.join(__dirname, './puzzleInput.txt'), 'utf8')).split(/\n/).map(i => parseInt(i, 10))
+  return readFile(path.join(__dirname, './puzzleInput.txt'), 'utf8')
 }
 
-describe('day XXX', () => {
-  describe('part 1', () => {
-    test('example', () => {
-      const input = []
-      const result = part1(input)
-      const answer = true
-      expect(result).toEqual(answer)
+describe('day 9', () => {
+  describe('part 1 and 2', () => {
+    test('example 1', () => {
+      const input = '{{{},{},{{}}}}'
+      const result = main(input)
+      const answer = 16
+      expect(result.score).toEqual(answer)
     })
 
     test('puzzle', async () => {
       const input = await puzzleInput()
-      const result = part1(input)
-      const answer = true
-      expect(result).toEqual(answer)
-    })
-  })
-
-  describe('part 2', () => {
-    test('example', () => {
-      const input = []
-      const result = part2(input)
-      const answer = true
-      expect(result).toEqual(answer)
-    })
-
-    test('puzzle', async () => {
-      const input = await puzzleInput()
-      const result = part2(input)
-      const answer = true
-      expect(result).toEqual(answer)
+      const result = main(input)
+      const answer = {
+        score: 16021,
+        garbageCount: 7685
+      }
+      expect(result.score).toEqual(answer.score)
+      expect(result.garbageCount).toEqual(answer.garbageCount)
     })
   })
 })
