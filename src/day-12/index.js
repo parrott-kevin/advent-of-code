1890b142c5d2cbde159b3b2f8a75acd31ad84055
Kevin Parrott <parrott.kevin@gmail.com>
2017-12-12 00:40:27 -0500

    day 12 completed

--- src/day-12/index.js
+++ src/day-12/index.js
@@ -1,9 +1,46 @@
-const part1 = () => {
-  return true
+function depthFirstIterative (graph, root) {
+  const visited = [root]
+  let stack = [...graph[root]]
+
+  while (stack.length > 0) {
+    const node = stack.pop()
+    if (visited.indexOf(node) < 0) {
+      visited.push(node)
+      stack = [...stack, ...graph[node]]
+    }
+  }
+
+  return visited
 }
 
-const part2 = () => {
-  return true
+const parse = (ary) => {
+  return ary.reduce((storage, i) => {
+    const s = i.split('<->')
+    const id = parseInt(s[0], 10)
+    const comms = s[1].split(',').map(i => parseInt(i, 10))
+    !(id in storage) && (storage[id] = comms)
+    return storage
+  }, {})
+}
+
+const part1 = (input) => {
+  const programs = parse(input)
+  return (depthFirstIterative(programs, 0)).length
+}
+
+const part2 = (input) => {
+  const programs = parse(input)
+  let visited = []
+  let groups = []
+  for (let key in programs) {
+    const id = parseInt(key, 10)
+    if (visited.indexOf(id) < 0) {
+      const dfi = depthFirstIterative(programs, id, 10)
+      visited = [...visited, ...dfi]
+      groups.push(dfi)
+    }
+  }
+  return groups.length
 }
 
 module.exports = { part1, part2 }
--- src/day-12/index.test.js
+++ src/day-12/index.test.js
@@ -6,38 +6,54 @@ const { part1, part2 } = require('./index')
 
 const readFile = util.promisify(fs.readFile)
 const puzzleInput = async () => {
-  return (await readFile(path.join(__dirname, './puzzleInput.txt'), 'utf8')).split(/\n/).map(i => parseInt(i, 10))
+  return (await readFile(path.join(__dirname, './puzzleInput.txt'), 'utf8')).split(/\n/)
 }
 
-describe('day XXX', () => {
+describe('day 12', () => {
   describe('part 1', () => {
     test('example', () => {
-      const input = []
+      const input = [
+        '0 <-> 2',
+        '1 <-> 1',
+        '2 <-> 0, 3, 4',
+        '3 <-> 2, 4',
+        '4 <-> 2, 3, 6',
+        '5 <-> 6',
+        '6 <-> 4, 5'
+      ]
       const result = part1(input)
-      const answer = true
+      const answer = 6
       expect(result).toEqual(answer)
     })
 
     test('puzzle', async () => {
       const input = await puzzleInput()
       const result = part1(input)
-      const answer = true
+      const answer = 175
       expect(result).toEqual(answer)
     })
   })
 
   describe('part 2', () => {
     test('example', () => {
-      const input = []
+      const input = [
+        '0 <-> 2',
+        '1 <-> 1',
+        '2 <-> 0, 3, 4',
+        '3 <-> 2, 4',
+        '4 <-> 2, 3, 6',
+        '5 <-> 6',
+        '6 <-> 4, 5'
+      ]
       const result = part2(input)
-      const answer = true
+      const answer = 2
       expect(result).toEqual(answer)
     })
 
     test('puzzle', async () => {
       const input = await puzzleInput()
       const result = part2(input)
-      const answer = true
+      const answer = 213
       expect(result).toEqual(answer)
     })
   })
