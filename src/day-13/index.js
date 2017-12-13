e9c4d28fba600966d65644a608e7f433923a69a3
Kevin Parrott <parrott.kevin@gmail.com>
2017-12-13 08:36:38 -0500

    day 13 part 1 complete, part 2 close

--- src/day-13/index.js
+++ src/day-13/index.js
@@ -1,9 +1,52 @@
-const part1 = () => {
-  return true
+const part1 = (firewall, maxDepth) => {
+  let severity = 0
+  let packetPosition = 0
+  for (let i = 0; i <= maxDepth; i++) {
+    if (packetPosition in firewall) {
+      const depth = firewall[packetPosition]
+      const x = i % (2 * (depth.range - 1))
+      // absolute value function y = a * abs(x - c) + d
+      const rangePosition = -1 * Math.abs(x - (depth.range - 1)) + (depth.range - 1)
+      if (rangePosition === 0) {
+        severity += packetPosition * depth.range
+      }
+    }
+    packetPosition += 1
+  }
+  return severity
 }
 
-const part2 = () => {
-  return true
+const part2 = (firewall, maxDepth) => {
+  let done = false
+  let delay = 0
+  let packetPosition = 0
+  while (!done) {
+    let caught = false
+    for (let i = 0; i <= (maxDepth - delay); i++) {
+      if (packetPosition >= 0) {
+        if (packetPosition in firewall) {
+          const depth = firewall[packetPosition]
+          const x = i % (2 * (depth.range - 1))
+          // absolute value function y = a * abs(x - c) + d
+          const rangePosition = -1 * Math.abs(x - (depth.range - 1)) + (depth.range - 1)
+          if (rangePosition === 0) {
+            caught = true
+            break
+          }
+        }
+      }
+      // console.log(i, packetPosition, (packetPosition in firewall), caught)
+      packetPosition += 1
+    }
+    if (caught) {
+      delay -= 1
+      packetPosition = delay
+    } else {
+      done = true
+    }
+  }
+
+  return Math.abs(delay)
 }
 
 module.exports = { part1, part2 }
--- src/day-13/index.test.js
+++ src/day-13/index.test.js
@@ -6,38 +6,79 @@ const { part1, part2 } = require('./index')
 
 const readFile = util.promisify(fs.readFile)
 const puzzleInput = async () => {
-  return (await readFile(path.join(__dirname, './puzzleInput.txt'), 'utf8')).split(/\n/).map(i => parseInt(i, 10))
+  return (await readFile(path.join(__dirname, './puzzleInput.txt'), 'utf8')).split(/\n/).reduce((storage, i) => {
+    const row = i.split(':')
+    storage[row[0]] = {
+      range: row[1],
+      position: 0
+    }
+    return storage
+  }, {})
 }
 
-describe('day XXX', () => {
+describe('day 13', () => {
   describe('part 1', () => {
     test('example', () => {
-      const input = []
-      const result = part1(input)
-      const answer = true
+      const input = {
+        0: {
+          range: 3,
+          position: 0
+        },
+        1: {
+          range: 2,
+          position: 0
+        },
+        4: {
+          range: 4,
+          position: 0
+        },
+        6: {
+          range: 4,
+          position: 0
+        }
+      }
+      const result = part1(input, parseInt(Object.keys(input).pop(), 10))
+      const answer = 24
       expect(result).toEqual(answer)
     })
 
     test('puzzle', async () => {
       const input = await puzzleInput()
-      const result = part1(input)
-      const answer = true
+      const result = part1(input, parseInt(Object.keys(input).pop(), 10))
+      const answer = 1960
       expect(result).toEqual(answer)
     })
   })
 
   describe('part 2', () => {
     test('example', () => {
-      const input = []
-      const result = part2(input)
-      const answer = true
+      const input = {
+        0: {
+          range: 3,
+          position: 0
+        },
+        1: {
+          range: 2,
+          position: 0
+        },
+        4: {
+          range: 4,
+          position: 0
+        },
+        6: {
+          range: 4,
+          position: 0
+        }
+      }
+      const result = part2(input, parseInt(Object.keys(input).pop(), 10))
+      const answer = 10
       expect(result).toEqual(answer)
     })
 
     test('puzzle', async () => {
       const input = await puzzleInput()
-      const result = part2(input)
-      const answer = true
+      const result = part2(input, parseInt(Object.keys(input).pop(), 10))
+      const answer = 10
       expect(result).toEqual(answer)
     })
   })
